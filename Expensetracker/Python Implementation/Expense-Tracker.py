f2e03be5a2ec972f915421a46c6d910d7553504f
ArsalanMoravvej <moravvejmohammad97@gmail.com>
2025-02-04 02:33:44 +0330

    Update: Updating Comand Handler Added.

--- Expensetracker/Python Implementation/Expense-Tracker.py
+++ Expensetracker/Python Implementation/Expense-Tracker.py
@@ -6,6 +6,12 @@ def add_expense(args):
 def summarize_expenses(args):
     print(args)
 
+def update_expense(args):
+    if args.description is None and args.amount is None:
+        print("Error: At least one of --description or --amount must be provided.")
+        exit(1)
+    print(args)
+
 def main():
     """
     Main function to set up the command-line interface and handle user commands.
@@ -21,9 +27,14 @@ def main():
 
     #Summarizing command handler
     summary_parser = subparsers.add_parser('summary', help="Summarize Expenses")
-    summary_parser.add_argument('--month', type=int, help="Summarize By Month Number")
+    summary_parser.add_argument('--month', type=int, help="Number of the Month")
     summary_parser.set_defaults(func=summarize_expenses)
 
+    #Updating command handler
+    update_parser = subparsers.add_parser('update', help="Update Expenses")
+    update_parser.add_argument('--description',type=str, help="Expense Description")
+    update_parser.add_argument('--amount', type=float , help="Expense Amount")
+    update_parser.set_defaults(func=update_expense)
 
     # Parse arguments and call the appropriate function
     args = parser.parse_args()
