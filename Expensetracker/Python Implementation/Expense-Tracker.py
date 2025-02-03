92b782bf7846b283b1a6d31225f0caa819a86705
ArsalanMoravvej <moravvejmohammad97@gmail.com>
2025-02-04 01:35:30 +0330

    added a boilerplate for expense tracker project.

---
+++ Expensetracker/Python Implementation/Expense-Tracker.py
@@ -0,0 +1,11 @@
+import argparse
+
+def main():
+    """
+    Main function to set up the command-line interface and handle user commands.
+    """
+
+    pass
+
+if __name__ == "__main__":
+    main()
