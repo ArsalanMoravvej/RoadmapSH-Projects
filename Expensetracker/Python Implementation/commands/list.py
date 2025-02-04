dad332fb6132931fa5bea04d1fb27248ca52fe88
ArsalanMoravvej <moravvejmohammad97@gmail.com>
2025-02-04 16:49:35 +0330

    Update: completed the functionality of  listing the expenses, now going for the remaining [update, delete, summarize]

--- Expensetracker/Python Implementation/commands/list.py
+++ Expensetracker/Python Implementation/commands/list.py
@@ -1,7 +1,18 @@
 from .db import expenses_collection
-from prettytable import prettytable
+from prettytable import PrettyTable
 
 def list_expenses(args):
     expenses = expenses_collection.find({"isDeleted": False})
 
-    print(list(expenses))
+    table = PrettyTable(["ID", "Date", "Description", "Amount"])
+    table.border = False
+
+    for expense in expenses:
+        table.add_row([
+            expense.get("_id", "N/A"),
+            expense["createdAt"].date(),
+            expense["description"],
+            "$" + str(expense["amount"])
+        ])
+    
+    print(table)
