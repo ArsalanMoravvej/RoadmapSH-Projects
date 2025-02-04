18f6abdd924f2cfcb388c3e4f69c253d38f0eaa2
ArsalanMoravvej <moravvejmohammad97@gmail.com>
2025-02-04 18:22:38 +0330

    Update: deleting a record functionality was successfully added to the system. now going for the last one that is summary

--- Expensetracker/Python Implementation/commands/delete.py
+++ Expensetracker/Python Implementation/commands/delete.py
@@ -1,4 +1,21 @@
-
+from .db import expenses_collection
+from datetime import datetime, timezone
 
 def delete_expense(args):
-    print(args)
+    """
+    Deletes an expense by marking it as deleted (soft delete).
+    """
+    result = expenses_collection.update_one(
+        {"_id": args.id},
+        {"$set": {"isDeleted": True}}
+    )
+
+    if result.matched_count == 0:
+        print(f"Expense with ID {args.id}, couldn't be found.")
+    elif result.modified_count == 0:
+        print(f"Expense with ID {args.id} was already deleted.")
+    else:
+        expenses_collection.update_one(
+            {"_id": args.id},
+            {"$set": {"modifiedAt": datetime.now(timezone.utc)}})
+        print(f"Expense with ID {args.id} was deleted successfully.")
