7c89e32291f64f754efeead846c98bd046753c2c
ArsalanMoravvej <moravvejmohammad97@gmail.com>
2025-02-12 16:42:57 +0330

    TaskTrackerAPI: added the logic of retreiving a post

--- TaskTackerAPI/TaskTrackerAPI-Py/app/routes/task.py
+++ TaskTackerAPI/TaskTrackerAPI-Py/app/routes/task.py
@@ -28,3 +28,42 @@ async def create_task(post: schemas.TaskCreate,
     db.refresh(new_task)
 
     return new_task
+
+# Retrieve a specific post by ID
+@router.get("/{id}", response_model=schemas.TaskResponse)
+async def get_task(id: int,
+                   db: Session = Depends(get_db),
+                   current_user: models.User = Depends(oauth2.get_current_user)):
+
+    task = db.query(models.Task).filter(models.Task.id == id).first()
+    
+    if not task:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Task with ID {id} was not found.")
+    
+    if task.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail=f"Not authorized to perform requested action on task with ID: {id}.")
+    
+    return task
+
+# Delete a post by ID
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_post(id: int,
+                      db:Session = Depends(get_db),
+                      current_user: int = Depends(oauth2.get_current_user)):
+    
+    task = db.query(models.Task).filter(models.Task.id == id)
+    
+    if task.first() is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with ID {id} was not found.")
+    
+    if task.first().owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail=f"Not authorized to perform requested action of task with ID: {id}.")
+    
+    task.delete(synchronize_session=False)
+    db.commit()
+    
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
