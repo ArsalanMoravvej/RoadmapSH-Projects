bbe122c2cea4847d3a7bf1ca4f4262bdf88f52fe
ArsalanMoravvej <moravvejmohammad97@gmail.com>
2025-02-12 10:55:26 +0330

    TaskTrackerAPI: adding routes (users.py + __init__.py)

---
+++ TaskTackerAPI/TaskTrackerAPI-Py/app/routes/user.py
@@ -0,0 +1,26 @@
+from fastapi  import status, HTTPException, Depends, APIRouter
+from .. import models, schemas, utils
+
+from ..database import get_db
+from sqlalchemy.orm import Session
+
+router = APIRouter(
+    prefix='/users',
+    tags=['Users']
+)
+
+#User Registration Path
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserResponse)
+async def create_user(user: schemas.UserCreate, db:Session = Depends(get_db)):
+
+    #hash the password
+    hashed_pass = utils.hash(user.password)
+    user.password = hashed_pass
+
+    new_user = models.User(**user.model_dump())
+    
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
