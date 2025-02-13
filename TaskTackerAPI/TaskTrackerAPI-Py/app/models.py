1bcaadfe953b57588735fa94658180462ac6940c
ArsalanMoravvej <moravvejmohammad97@gmail.com>
2025-02-13 19:44:17 +0330

    TaskTrackerAPI: Added columns [priority and status] to tasks table.

--- TaskTackerAPI/TaskTrackerAPI-Py/app/models.py
+++ TaskTackerAPI/TaskTrackerAPI-Py/app/models.py
@@ -8,12 +8,14 @@ from .database import Base
 class Task(Base):
     __tablename__ = "tasks"
 
-    id         = Column(Integer, primary_key=True, nullable=False)
-    title      = Column(String, nullable=False)
-    description    = Column(String, nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True),
+    id          = Column(Integer, primary_key=True, nullable=False)
+    title       = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    status      = Column(Integer, nullable=False, server_default="1")
+    priority    = Column(Integer, nullable=False, server_default="5")
+    created_at  = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default = text('now()'))
-    owner_id = Column(Integer, ForeignKey("users.id", 
+    owner_id    = Column(Integer, ForeignKey("users.id", 
                                           ondelete="CASCADE"), nullable=False)
 
     owner = relationship("User")
