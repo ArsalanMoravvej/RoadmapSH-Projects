76873066816eb2438e35d3f8f30ad6d0e2ac7ef5
ArsalanMoravvej <moravvejmohammad97@gmail.com>
2025-02-14 17:05:50 +0330

    TaskTrackerAPI: added columns for soft deletion manging and as well as a table for tracking the last modification dateandtime.

--- TaskTackerAPI/TaskTrackerAPI-Py/app/models.py
+++ TaskTackerAPI/TaskTrackerAPI-Py/app/models.py
@@ -8,15 +8,16 @@ from .database import Base
 class Task(Base):
     __tablename__ = "tasks"
 
-    id          = Column(Integer, primary_key=True, nullable=False)
-    title       = Column(String, nullable=False)
-    description = Column(String, nullable=False)
-    status      = Column(Integer, nullable=False, server_default="1")
-    priority    = Column(Integer, nullable=False, server_default="5")
-    created_at  = Column(TIMESTAMP(timezone=True),
-                        nullable=False, server_default = text('now()'))
-    owner_id    = Column(Integer, ForeignKey("users.id", 
-                                          ondelete="CASCADE"), nullable=False)
+    id               = Column(Integer, primary_key=True, nullable=False)
+    title            = Column(String, nullable=False)
+    description      = Column(String, nullable=False)
+    status           = Column(Integer, nullable=False, server_default="1")
+    priority         = Column(Integer, nullable=False, server_default="5")
+    is_deleted       = Column(Boolean, nullable=False, server_default=text("false"))
+    deleted_at       = Column(TIMESTAMP(timezone=True), nullable=True)
+    last_modified_at = Column(TIMESTAMP(timezone=True), nullable=True)
+    created_at       = Column(TIMESTAMP(timezone=True), nullable=False, server_default = text('now()'))
+    owner_id         = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
 
     owner = relationship("User")
 
@@ -24,9 +25,11 @@ class Task(Base):
 class User(Base):
     __tablename__ = "users"
 
-    id         = Column(Integer, primary_key=True, nullable=False)
-    name       = Column(String, nullable=False)
-    email      = Column(String, nullable=False, unique=True)
-    password   = Column(String, nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True),
-                        nullable=False, server_default = text('now()'))
+    id               = Column(Integer, primary_key=True, nullable=False)
+    name             = Column(String, nullable=False)
+    email            = Column(String, nullable=False, unique=True)
+    password         = Column(String, nullable=False)
+    is_deleted       = Column(Boolean, nullable=False, server_default=text("false"))
+    deleted_at       = Column(TIMESTAMP(timezone=True), nullable=True)
+    last_modified_at = Column(TIMESTAMP(timezone=True), nullable=True)
+    created_at       = Column(TIMESTAMP(timezone=True), nullable=False, server_default = text('now()'))
